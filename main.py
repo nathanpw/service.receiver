30c91d0a7e31e4a4f63d14ca0d82b0f49a100c36
Nathan Weir <nathanpweir@gmail.com>
2020-01-08 18:19:57 -0500

    fix: Move service to run after checking settings

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import xbmcaddon
 # Keep this file to a minimum, as Kodi
 # doesn't keep a compiled copy of this
 
-service.run()
-
 def main():
     # Check we have an ip and port configured.
     addon = xbmcaddon.Addon()
@@ -18,6 +16,8 @@ def main():
         addon.openSettings()
     ip_address = addon.getSetting('ip_address')
     port = addon.getSetting('port')
+
+    service.run()
 
 if (__name__ == "__main__"):
     main()
