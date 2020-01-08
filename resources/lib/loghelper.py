562ac196e75d5dc518e601ef909118a4ea55e64b
Nathan Weir <nathanpweir@gmail.com>
2020-01-08 18:05:38 -0500

    chore: remove uneeded comments and logging

--- resources/lib/loghelper.py
+++ resources/lib/loghelper.py
@@ -22,15 +22,9 @@ def log(msg_text, msg_type):
 
     if msg_type == 'VOLUME_CHANGE':
         addon_path = addon.getAddonInfo('path').decode("utf-8")
-        #xbmcgui.Dialog().notification(addon_path, 'addon_path: ' + str(addon_path), xbmcgui.NOTIFICATION_ERROR, 5000)
         image_num = addon.getSetting('icon_style')
-        #xbmcgui.Dialog().notification(addon_name, 'icon_style: ' + str(image_num), xbmcgui.NOTIFICATION_ERROR, 5000)
-        #xbmcgui.Dialog().notification(addon_name, 'icon_color: ' + str(addon.getSettingBool('icon_color')), xbmcgui.NOTIFICATION_ERROR, 5000)
         if addon.getSettingBool('icon_color'):
             icon = addon_path + "/media/icons/infodialogs/" + "VolBlack" + image_num  + ".png"
         else:
             icon = addon_path + "/media/icons/infodialogs/" + "VolWhite" + image_num + ".png"
-        xbmc.log('{0} - {1}'.format(addon_name, "icon: " + icon), level=xbmc.LOGDEBUG)
-        #xbmcgui.Dialog().notification(addon_name, 'icon: ' + str(icon), xbmcgui.NOTIFICATION_ERROR, 5000)
-        #xbmcgui.Dialog().notification(addon_name, 'icon path: ' + addon_path + "/media/icons/infodialogs/" + icon, xbmcgui.NOTIFICATION_ERROR, 5000)
         xbmcgui.Dialog().notification(addon_name, '{0}'.format(msg_text), icon, addon.getSettingInt('display_time'))
